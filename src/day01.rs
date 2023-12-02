600ef5189ab5df879ca3b94ff736d003a3970885
vintitres <vintitres@users.noreply.github.com>
2023-12-02 06:19:09 +0000

    1b

--- src/day01.rs
+++ src/day01.rs
@@ -9,8 +9,31 @@ pub fn part1(input: &str) -> u32 {
     ).sum()
 }
 
-pub fn part2(input: &str) -> usize {
-    input.len()
+pub fn part2(input: &str) -> u32 {
+    input.lines()
+    .map(
+        |l| {
+            l.replace("one", "one1one")
+            .replace("two", "two2two")
+            .replace("three", "three3three")
+            .replace("four", "four4four")
+            .replace("five", "five5five")
+            .replace("six", "six6six")
+            .replace("seven", "seven7seven")
+            .replace("eight", "eight8eight")
+            .replace("nine", "nine9nine")
+            // .replace("zero", "0")
+        }
+    )
+    .map(
+        |l| {
+            let mut digits = l.chars().flat_map(|c| c.to_digit(10));
+            let f = digits.next().unwrap();
+            let l = digits.last().unwrap_or(f);
+            dbg!(f, l);
+            f * 10 + l
+        }
+    ).sum()
 }
 
 #[cfg(test)]

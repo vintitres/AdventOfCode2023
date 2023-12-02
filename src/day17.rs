9ef5655069d7838f67aaaa2fc0c84b2c4484ae4d
vintitres <vintitres@users.noreply.github.com>
2023-12-02 05:21:27 +0000

    scaffold days

---
+++ src/day17.rs
@@ -0,0 +1,29 @@
+pub fn part1(input: &str) -> usize {
+    input.len()
+}
+
+pub fn part2(input: &str) -> usize {
+    input.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn input() -> &'static str {
+        "input"
+        // include_str!("../input/2023/day01.txt")
+    }
+
+    #[ignore = "not implemented"]
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(input()), 11);
+    }
+
+    #[ignore = "not implemented"]
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(input()), 22);
+    }
+}

d3e1cd4a56f878d6aac33a1aa06c717fa1a6f81f
vintitres <vintitres@users.noreply.github.com>
2023-12-02 06:23:10 +0000

    s2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let input = include_str!("../input/2023/day1.txt");
-    dbg!(advent_of_code_2023::day01::part2(input));
+    let input = include_str!("../input/2023/day2.txt");
+    dbg!(advent_of_code_2023::day02::part1(input));
 }

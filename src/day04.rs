3064996899c452e4f5068ffc53888f7ac3cc8d34
vintitres <vintitres@users.noreply.github.com>
2023-12-04 05:43:16 +0000

    4a

--- src/day04.rs
+++ src/day04.rs
@@ -1,5 +1,29 @@
-pub fn part1(input: &str) -> usize {
-    input.len()
+use itertools::Itertools;
+use std::collections::HashSet;
+
+pub fn part1(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|l| {
+            let (win, my) = l
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(" | ")
+                .map(|nums| {
+                    HashSet::<u64>::from_iter(nums.split(' ').flat_map(|n| n.parse::<u64>()))
+                })
+                .collect_tuple()
+                .unwrap();
+            let mywin = my.intersection(&win).collect_vec();
+            let l = mywin.len();
+            if l == 0 {
+                0
+            } else {
+                2u64.pow(l.saturating_sub(1) as u32)
+            }
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,14 +35,12 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        "input"
-        // include_str!("../input/2023/day01.txt")
+        include_str!("../input/2023/day4.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 11);
+        assert_eq!(part1(input()), 23441);
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let input = include_str!("../input/2023/day3.txt");
-    dbg!(advent_of_code_2023::day03::part2(input));
+    let input = include_str!("../input/2023/day4.txt");
+    dbg!(advent_of_code_2023::day04::part1(input));
 }

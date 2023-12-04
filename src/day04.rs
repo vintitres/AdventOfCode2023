c2929b660b6bd140d3ef2506185abf20b9200356
vintitres <vintitres@users.noreply.github.com>
2023-12-04 05:58:53 +0000

    4

--- src/day04.rs
+++ src/day04.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 pub fn part1(input: &str) -> u64 {
     input
@@ -27,7 +27,34 @@ pub fn part1(input: &str) -> u64 {
 }
 
 pub fn part2(input: &str) -> usize {
-    input.len()
+    let cards = input
+        .lines()
+        .map(|l| {
+            let (win, my) = l
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(" | ")
+                .map(|nums| {
+                    HashSet::<u64>::from_iter(nums.split(' ').flat_map(|n| n.parse::<u64>()))
+                })
+                .collect_tuple()
+                .unwrap();
+            my.intersection(&win).collect_vec().len()
+        })
+        .collect_vec();
+    let mut card_count = 0;
+    let mut q = VecDeque::from_iter(0..cards.len());
+    while !q.is_empty() {
+        card_count += 1;
+        let card = q.pop_front().unwrap();
+        // dbg!(card);
+        for new_card in (card + 1)..(card + 1 + cards[card]) {
+            q.push_back(new_card);
+            // dbg!(new_card);
+        }
+    }
+    card_count
 }
 
 #[cfg(test)]
@@ -43,9 +70,8 @@ mod tests {
         assert_eq!(part1(input()), 23441);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 22);
+        assert_eq!(part2(input()), 5923918);
     }
 }

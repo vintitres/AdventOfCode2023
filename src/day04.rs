10b71e8e42e6a83ba50f5f3eb8000b14f4a150b4
vintitres <vintitres@users.noreply.github.com>
2023-12-04 06:11:52 +0000

    fn

--- src/day04.rs
+++ src/day04.rs
@@ -1,33 +1,8 @@
 use itertools::Itertools;
 use std::collections::HashSet;
 
-pub fn part1(input: &str) -> u64 {
+fn read_cards(input: &str) -> Vec<usize> {
     input
-        .lines()
-        .map(|l| {
-            let (win, my) = l
-                .split(": ")
-                .nth(1)
-                .unwrap()
-                .split(" | ")
-                .map(|nums| {
-                    HashSet::<u64>::from_iter(nums.split(' ').flat_map(|n| n.parse::<u64>()))
-                })
-                .collect_tuple()
-                .unwrap();
-            let mywin = my.intersection(&win).collect_vec();
-            let l = mywin.len();
-            if l == 0 {
-                0
-            } else {
-                2u64.pow(l.saturating_sub(1) as u32)
-            }
-        })
-        .sum()
-}
-
-pub fn part2(input: &str) -> usize {
-    let cards = input
         .lines()
         .map(|l| {
             let (win, my) = l
@@ -42,7 +17,24 @@ pub fn part2(input: &str) -> usize {
                 .unwrap();
             my.intersection(&win).collect_vec().len()
         })
-        .collect_vec();
+        .collect_vec()
+}
+
+pub fn part1(input: &str) -> u64 {
+    read_cards(&input)
+        .iter()
+        .map(|l| {
+            if *l == 0 {
+                0
+            } else {
+                2u64.pow(l.saturating_sub(1) as u32)
+            }
+        })
+        .sum()
+}
+
+pub fn part2(input: &str) -> usize {
+    let cards = read_cards(input);
     let mut card_count = 0;
     let mut owned_cards = cards.iter().map(|_| 1).collect_vec();
     for card in 0..owned_cards.len() {

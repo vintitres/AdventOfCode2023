6456c54d5ec1bb6683c4b7fd222c2608f594bb24
vintitres <vintitres@users.noreply.github.com>
2023-12-08 06:40:33 +0000

    7b

--- src/day07.rs
+++ src/day07.rs
@@ -9,29 +9,36 @@ struct Hand {
 }
 
 impl Hand {
-    fn parse(hand: &str) -> Hand {
-        let mut handmap: BTreeMap<char, u64> = BTreeMap::new();
+    fn parse(hand: &str, jokers: bool) -> Hand {
+        let mut handmap: BTreeMap<char, usize> = BTreeMap::new();
         for c in hand.chars() {
             handmap.entry(c).and_modify(|v| *v = *v + 1).or_insert(1);
         }
-        let type_ = if *handmap.iter().next().unwrap().1 == 5 {
+        let jokers_count = if jokers {
+            handmap.remove(&'J').unwrap_or(0)
+        } else {
+            0
+        };
+        let type_ = if jokers_count == 5 || *handmap.iter().next().unwrap().1 + jokers_count == 5 {
             // five of kind
             0
-        } else if handmap.iter().any(|(_, &c)| c == 4) {
+        } else if jokers_count == 4 || handmap.iter().any(|(_, &c)| c == 4 - jokers_count) {
             // four of kind
             1
-        } else if handmap.iter().any(|(_, &c)| c == 3) {
-            if handmap.iter().any(|(_, &c)| c == 2) {
+        } else if jokers_count == 3 || handmap.iter().any(|(_, &c)| c == 3 - jokers_count) {
+            if handmap.iter().any(|(_, &c)| c == 2)
+                && (jokers_count != 1 || handmap.iter().filter(|(_, &c)| c == 2).count() == 2)
+            {
                 // full house
                 2
             } else {
                 // three of a kind
                 3
             }
-        } else if handmap.iter().filter(|(_, &c)| c == 2).count() == 2 {
+        } else if handmap.iter().filter(|(_, &c)| c == 2).count() >= 2 - jokers_count {
             // two pair
             4
-        } else if handmap.iter().any(|(_, &c)| c == 2) {
+        } else if jokers_count == 1 || handmap.iter().any(|(_, &c)| c == 2) {
             // pair
             5
         } else {
@@ -44,7 +51,13 @@ impl Hand {
                 'A' => 0,
                 'K' => 1,
                 'Q' => 2,
-                'J' => 3,
+                'J' => {
+                    if jokers {
+                        13
+                    } else {
+                        3
+                    }
+                }
                 'T' => 4,
                 '9' => 5,
                 '8' => 6,
@@ -56,18 +69,18 @@ impl Hand {
                 '2' => 12,
                 _ => unimplemented!(),
             })
-            .fold(0, |acc, n| acc * 13 + n);
+            .fold(0, |acc, n| acc * 14 + n);
         Hand { type_, val }
     }
 }
 
-pub fn part1(input: &str) -> u64 {
+fn doit(input: &str, jokers: bool) -> u64 {
     let mut hands = input
         .lines()
         .map(|l| {
             let (hand, bet) = l.split_once(' ').unwrap();
             let bet = bet.parse::<u64>().unwrap();
-            let hand = Hand::parse(hand);
+            let hand = Hand::parse(hand, jokers);
             (hand, bet)
         })
         .collect_vec();
@@ -84,8 +97,12 @@ pub fn part1(input: &str) -> u64 {
         .sum()
 }
 
-pub fn part2(input: &str) -> usize {
-    input.len()
+pub fn part1(input: &str) -> u64 {
+    doit(input, false)
+}
+
+pub fn part2(input: &str) -> u64 {
+    doit(input, true)
 }
 
 #[cfg(test)]
@@ -101,9 +118,8 @@ mod tests {
         assert_eq!(part1(input()), 251287184);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 22);
+        assert_eq!(part2(input()), 250757288);
     }
 }

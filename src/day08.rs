47d40077eb8c1fc2c287a6cef4b9ef0c18b9eeed
vintitres <vintitres@users.noreply.github.com>
2023-12-08 07:09:46 +0000

    8a

--- src/day08.rs
+++ src/day08.rs
@@ -1,5 +1,39 @@
+use std::collections::HashMap;
+
 pub fn part1(input: &str) -> usize {
-    input.len()
+    let mut lines = input.lines();
+    let rl = lines.next().unwrap();
+    lines.next();
+    let map: HashMap<&str, (&str, &str)> = lines
+        .map(|l| {
+            let (node, tonodes) = l.split_once(" = (").unwrap();
+            let (l, r) = tonodes.split_once(", ").unwrap();
+            let r = r.strip_suffix(')').unwrap();
+            (node, (l, r))
+        })
+        .collect();
+    rl.chars()
+        .cycle()
+        .scan("AAA", |node, rl| {
+            // dbg!(&node);
+            if *node == "ZZZ" {
+                None
+            } else {
+                let nodem = map[node];
+                match rl {
+                    'R' => {
+                        *node = nodem.1;
+                        Some(rl)
+                    }
+                    'L' => {
+                        *node = nodem.0;
+                        Some(rl)
+                    }
+                    _ => unreachable!(""),
+                }
+            }
+        })
+        .count()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,14 +45,12 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        "input"
-        // include_str!("../input/2023/day01.txt")
+        include_str!("../input/2023/day8.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 11);
+        assert_eq!(part1(input()), 15517);
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let input = include_str!("../input/2023/day7e.txt");
-    dbg!(advent_of_code_2023::day07::part1(input));
+    let input = include_str!("../input/2023/day8e.txt");
+    dbg!(advent_of_code_2023::day08::part1(input));
 }

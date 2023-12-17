9fc3205446a307b6e5a780cb3efe545f04fb5a12
vintitres <vintitres@users.noreply.github.com>
2023-12-17 23:42:26 +0000

    10bugh

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,8 @@
 use itertools::Itertools;
-use std::collections::{HashMap, VecDeque};
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    iter,
+};
 
 fn mv((px, py): (usize, usize), d: char) -> (usize, usize) {
     match d {
@@ -19,7 +22,7 @@ fn get(pipes: &Vec<Vec<char>>, (px, py): (usize, usize)) -> char {
     }
 }
 
-pub fn part1(input: &str) -> usize {
+fn doit(input: &str) -> (usize, usize) {
     let pipes = input.lines().map(|l| l.chars().collect_vec()).collect_vec();
     let s = pipes
         .iter()
@@ -29,7 +32,7 @@ pub fn part1(input: &str) -> usize {
     let mut q = VecDeque::new();
     let mut vis = HashMap::new();
     vis.insert(s, 0);
-    ['N', 'S'] //, 'E', 'W']  // input
+    ['N', 'S'] //, 'E', 'W']  // looked at input :/
         .iter()
         .enumerate()
         .for_each(|(i, &d)| {
@@ -41,7 +44,6 @@ pub fn part1(input: &str) -> usize {
     let mut maxd = 0;
     while !q.is_empty() {
         let (dis, p, n) = q.pop_front().unwrap();
-        dbg!((dis, p, n));
         match get(&pipes, p) {
             // 'S' => vec!['N', 'S', 'E', 'W'],
             '|' => vec!['N', 'S'],
@@ -67,6 +69,7 @@ pub fn part1(input: &str) -> usize {
             }
         });
     }
+
     for (i, _) in pipes.iter().enumerate() {
         for (j, _) in pipes[i].iter().enumerate() {
             print!(
@@ -79,11 +82,90 @@ pub fn part1(input: &str) -> usize {
         }
         println!();
     }
-    maxd
+    let mut q = VecDeque::new();
+    let mut vis2 = HashSet::new();
+    q.push_back((0, 0));
+    vis2.insert((0, 0));
+    while !q.is_empty() {
+        let p = q.pop_front().unwrap();
+        ['N', 'S', 'E', 'W']
+            .iter()
+            .map(|&d| mv(p, d))
+            .for_each(|p| {
+                if !vis2.contains(&p) {
+                    if p.0 < pipes.len() * 3 && p.1 < pipes[p.0 / 3].len() * 3 {
+                        if !vis.contains_key(&(p.0 / 3, p.1 / 3))
+                            || !match pipes[p.0 / 3][p.1 / 3] {
+                                '|' | 'S' => [(0, 1), (1, 1), (2, 1)],
+                                '-' => [(1, 0), (1, 1), (1, 2)],
+                                'L' => [(0, 1), (1, 1), (1, 2)],
+                                '7' => [(1, 0), (1, 1), (2, 1)],
+                                'F' => [(2, 1), (1, 1), (1, 2)],
+                                'J' => [(1, 0), (1, 1), (0, 1)],
+                                _ => panic!("unknown"),
+                            }
+                            .iter()
+                            .any(|&xy| xy == (p.0 % 3, p.1 % 3))
+                        {
+                            q.push_back(p);
+                            vis2.insert(p);
+                        }
+                    }
+                }
+            });
+    }
+    /*
+    for i in 0..(pipes.len() * 3)  {
+        for j in 0..(pipes[i].len() * 3) {
+            if !vis
+
+        }
+    }
+     */
+    let mut inner = 0;
+    for (i, _) in pipes.iter().enumerate() {
+        for (j, _) in pipes[i].iter().enumerate() {
+            print!(
+                "{}",
+                match vis.get(&(i, j)) {
+                    Some(n) => "#".to_string(),
+                    None =>
+                        if ((i * 3)..((i + 1) * 3))
+                            .any(|i| ((j * 3)..((j + 1) * 3)).any(|j| vis2.contains(&(i, j))))
+                        {
+                            ".".to_string()
+                        } else {
+                            inner += 1;
+                            "I".to_string()
+                        },
+                }
+            );
+        }
+        println!();
+    }
+    (maxd, inner)
+    /*
+        pipes
+            .iter()
+            .enumerate()
+            .map(|(i, pp)| {
+                pp.iter()
+                    .enumerate()
+                    .filter(|(j, _)| {
+                        ((i * 3)..((i + 1) * 3))
+                            .any(|i| ((j * 3)..((j + 1) * 3)).any(|j| vis2.contains(&(i, j))))
+                    })
+                    .count()
+            })
+            .sum(),
+    )*/
+}
+pub fn part1(input: &str) -> usize {
+    doit(input).0
 }
 
 pub fn part2(input: &str) -> usize {
-    input.len()
+    doit(input).1
 }
 
 #[cfg(test)]
@@ -99,9 +181,8 @@ mod tests {
         assert_eq!(part1(input()), 7093);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 22);
+        assert_eq!(part2(input()), 23);
     }
 }

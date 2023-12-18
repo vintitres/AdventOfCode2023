020bb87a3159f95fe914b2647340dce592da9d37
vintitres <vintitres@users.noreply.github.com>
2023-12-18 21:44:52 +0000

    12a

--- src/day12.rs
+++ src/day12.rs
@@ -1,5 +1,63 @@
+use itertools::Itertools;
+
+struct HotSpringRow {
+    row: Vec<char>,
+    conditions: Vec<usize>,
+}
+
+impl HotSpringRow {
+    pub fn parse(s: &str) -> Self {
+        let (row, conditions) = s.split_ascii_whitespace().collect_tuple().unwrap();
+        Self {
+            row: row.chars().collect_vec(),
+            conditions: conditions.split(',').flat_map(|n| n.parse()).collect_vec(),
+        }
+    }
+
+    pub fn combinations(&self) -> usize {
+        Self::combinations_(self.row.split_first(), &self.conditions, 0)
+    }
+
+    fn combinations_(
+        row: Option<(&char, &[char])>,
+        conditions: &[usize],
+        springs_just_before: usize,
+    ) -> usize {
+        match row {
+            None => {
+                if conditions.len() <= 1 && *conditions.first().unwrap_or(&0) == springs_just_before
+                {
+                    1
+                } else {
+                    0
+                }
+            }
+            Some(('.', row)) => {
+                if springs_just_before == 0 {
+                    Self::combinations_(row.split_first(), conditions, 0)
+                } else if *conditions.first().unwrap_or(&0) == springs_just_before {
+                    Self::combinations_(row.split_first(), &conditions[1..], 0)
+                } else {
+                    0
+                }
+            }
+            Some(('#', row)) => {
+                Self::combinations_(row.split_first(), conditions, springs_just_before + 1)
+            }
+            Some(('?', row)) => {
+                Self::combinations_(Some((&'#', row)), conditions, springs_just_before)
+                    + Self::combinations_(Some((&'.', row)), conditions, springs_just_before)
+            }
+            _ => panic!("!"),
+        }
+    }
+}
 pub fn part1(input: &str) -> usize {
-    input.len()
+    input
+        .lines()
+        .map(HotSpringRow::parse)
+        .map(|hsr| hsr.combinations())
+        .sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,14 +69,12 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        "input"
-        // include_str!("../input/2023/day01.txt")
+        include_str!("../input/2023/day12.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 11);
+        assert_eq!(part1(input()), 8022);
     }
 
     #[ignore = "not implemented"]

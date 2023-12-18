b7e5d1cfa34a2f74a4c5457eaac1c95da3fd9975
vintitres <vintitres@users.noreply.github.com>
2023-12-18 01:09:07 +0000

    11b

--- src/day11.rs
+++ src/day11.rs
@@ -120,7 +120,6 @@ mod tests {
         assert_eq!(part1(input()), 10228230);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
         assert_eq!(part2(input()), 22);

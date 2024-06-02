31c77684d656c5d28d50f9144916736a3d5cb947
vintitres <vintitres@users.noreply.github.com>
2024-06-02 01:43:13 +0000

    up

--- src/day13.rs
+++ src/day13.rs
@@ -1,5 +1,21 @@
+use std::hash::{DefaultHasher, Hasher, Hash};
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+fn find_mirrors(hashes: &[u64]) -> Vec<usize> {
+    for i in 0..hashes.len() {
+
+    }
+    vec![0]
+} 
+
 pub fn part1(input: &str) -> usize {
-    input.len()
+    let lines_hashes: Vec<u64> = input.lines().map(|l| calculate_hash(&l)).collect();
+    find_mirrors(&lines_hashes).into_iter().sum()
 }
 
 pub fn part2(input: &str) -> usize {

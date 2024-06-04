92a655c030d751150100e5158d2de0c042363392
vintitres <vintitres@users.noreply.github.com>
2024-06-04 23:19:32 +0000

    15a

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,11 @@
-pub fn part1(input: &str) -> usize {
-    input.len()
+fn reindeer_hash(word: &str) -> u64 {
+    word.chars()
+        .filter(|c| *c != '\n')
+        .fold(0, |acc, c| ((acc + c as u64) * 17) % 256)
+}
+
+pub fn part1(input: &str) -> u64 {
+    input.split(',').map(reindeer_hash).sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,14 +17,12 @@ mod tests {
     use super::*;
 
     fn input() -> &'static str {
-        "input"
-        // include_str!("../input/2023/day01.txt")
+        include_str!("../input/2023/day15.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 11);
+        assert_eq!(part1(input()), 503487);
     }
 
     #[ignore = "not implemented"]
